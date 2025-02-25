1c8da2cb7971ebd94dada2ac10064d93d6573542
Vishal Srivastava <vsjee2002@gmail.com>
2025-02-25 16:15:46 +0530

    menubar-responsive

--- script.js
+++ script.js
@@ -11,4 +11,11 @@
         event.currentTarget.classList.add("active-link");
         document.getElementById(tabname).classList.add("active-tab");
     }
-    // sfsdn
+    
+    var sidemenu = document.getElementById("sidemenu");
+    function openmenu(){
+        sidemenu.style.right = "0";
+    }
+    function closemenu(){
+        sidemenu.style.right = "-200px";
+    }
